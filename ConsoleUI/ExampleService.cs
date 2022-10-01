8d71271fd1a28453551b4a5335f9405e33bb1086
Aly Badara Keita <alkeita0@gmail.com>
2022-10-01 13:39:13 +0100

    using a kind of dependancy injection with ConsoleApp

---
+++ ConsoleUI/ExampleService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+
+
+// Using Ingection Dependancy, Serilog and Settings
+
+namespace ConsoleUI
+{
+    public class ExampleService : IExampleService
+    {
+        private readonly ILogger<ExampleService> _log;
+        private readonly IConfiguration _config;
+
+        public ExampleService(ILogger<ExampleService> log, IConfiguration config)
+        {
+            _log = log;
+            _config = config;
+        }
+
+        public void Courir()
+        {
+            for (int i = 0; i < _config.GetValue<int>("LoopTimes"); i++)
+            {
+                _log.LogInformation("Run number {runNumber}", i);
+            }
+        }
+    }
+}
---
+++ ConsoleUI/IExampleService.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleUI
+{
+    public interface IExampleService
+    {
+        void Courir();
+    }
+}
